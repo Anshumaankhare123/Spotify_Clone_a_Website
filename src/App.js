b985a6974eeb5f5c356f029d18cd0a5e3e6b72d7
Anshumaan khare <94692766+Anshumaankhare123@users.noreply.github.com>
2023-12-13 21:23:55 +0530

    'commit'

---
+++ src/App.js
@@ -0,0 +1,365 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div class="sidebar">
+
+        <div class="logo">
+          <a href="#/"><img src="./images/logo.png" alt="logo"/></a>
+        </div>
+        <div class="navigation" id="upper">
+          <ul>
+            <li>
+              <a href="#/">
+                <i class="fa-solid fa-home"></i>
+                <span>Home</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="#/">
+                <i class="fa fa-search"></i>
+                <span>Search</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="#/">
+                <i class="fa fas fa-book"></i>
+                <span>Your library</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="navigation">
+          <ul>
+            <li>
+              <a href="#/">
+                <i class="fa fas fa-plus-square"></i>
+                <span>Create Playlist</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="#/">
+                <i class="fa fas fa-heart"></i>
+                <span>Liked Songs</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="policies">
+          <ul>
+            <li><a href="#/">Cookies</a></li>
+            <li><a href="#/">Privacy</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="main-container">
+        <div class="topbar">
+          <div class="prev-next-buttons">
+            <button type="button" class="fa fas fa-chevron-left"></button>
+            <button type="button" class="fa fas fa-chevron-right"></button>
+          </div>
+          <div class="navbar">
+            <ul>
+              <li><a href="#/">Premium</a></li>
+              <li><a href="#/">Support</a></li>
+              <li><a href="#/">Download</a></li>
+              <li class="divider">|</li>
+              <li><a href="#/">Sign Up</a></li>
+            </ul>
+            <button type="button">Login In</button>
+          </div>
+        </div>
+
+
+        <div class="spotify-playlist">
+          <h2>Spotify Playlists</h2>
+          <div class="list">
+            <div class="item">
+              <img src="./images/playList10.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Sour</h4>
+                <p>Song • Olivia Rodrigo</p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playList11.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Lover</h4>
+                <p>Song • Taylor Swift </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playList12.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+            <div class="item">
+              <img src="./images/playList14.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits of all times</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playlist15.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playList16.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playList9.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>2021 • Billie Eilish </p>
+
+            </div>
+
+
+
+          </div>
+        </div>
+        
+
+
+        <div class="spotify-playlist">
+          <h2>Chill</h2>
+          <div class="list">
+            <div class="item">
+              <img src="./images/Group25.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Justice</h4>
+                <p>2021 • Justin Bieber </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/image4.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>First Class</h4>
+                <p>Song • Jack Harlow </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/jack.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits of 2021</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+            <div class="item">
+              <img src="./images/2.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Punjabi 101</h4>
+                <p>By Spotify </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/3.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Bollywood Blast</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/wishingWall.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Wishing Well</h4>
+                <p>Juice WRLD</p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/topHit.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Blinding Lights</h4>
+                <p> Song • The Weekend</p>
+
+            </div>
+
+
+
+          </div>
+        </div>
+
+     
+
+        <div class="spotify-playlist">
+          <h2>Trending</h2>
+          <div class="list">
+            <div class="item">
+              <img src="./images/playList1.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playList2.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playList3.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+            <div class="item">
+              <img src="./images/playList4.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playList7.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playList5.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+            <div class="item">
+              <img src="./images/playList6.png" alt=""/>
+                <div class="play">
+                  <span class="fa fa-play"></span>
+                </div>
+                <h4>Top Hits</h4>
+                <p>Kick back to the best new and recent.... </p>
+
+            </div>
+
+
+
+          </div>
+        </div>
+
+        <hr/>
+          <div class="preview">
+            <div class="text">
+              <h6>Preview of Spotify</h6>
+              <p>Sign Up to get unlimited songs and podcasts with ocassional ads. No credit card needed.</p>
+            </div>
+            <div class="button">
+              <button type="button">Sign Up for free</button>
+            </div>
+          </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
